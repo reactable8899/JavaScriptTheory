e9c48e9fe852a2f969fba58197d00be46a4c566f
reactable8899 <olim.haydarov8899@gmail.com>
2020-04-26 21:52:17 +0500

    ss

--- miniTodolist/index.js
+++ miniTodolist/index.js
@@ -1,20 +1,20 @@
 const todo = function() {
   let button = document.getElementById('button');
   let ul = document.getElementById('ul')
+  let input = document.getElementById('input');
 
   function init() {
     button.addEventListener('click', function() {
-
-      let input = document.getElementById('input').value;
+      input = document.getElementById('input').value;
       let li = document.createElement('li');
 
         li.innerHTML = input;
         ul.append(li);
         ul.appendChild(li).classList.add('difference');
 
-        input = document.getElementById('input').value = '';
+        input.value = '';
+    });
 
-    })
   }
 
   return {
