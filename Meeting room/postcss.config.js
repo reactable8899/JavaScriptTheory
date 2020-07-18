8bd724aaca6c4617e78c29a1095089c6117042f4
reactable8899 <olim.haydarov8899@gmail.com>
2020-07-19 01:13:59 +0500

    rooms

---
+++ Meeting room/postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    'postcss-preset-env': {},
+    'postcss-nested': {},
+    //'cssnano': {},
+    'postcss-custom-properties': {},
+    'postcss-css-variables': {},
+  }
+}
---
+++ Meeting room/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+
+  context: path.join(__dirname, 'src'),
+  entry: {
+    index: './index.js',
+    styles: './css/style.css'
+  },
+  output: {
+    filename: '[name].js',
+    library: 'TodoList',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    path: path.join(__dirname, 'dist')
+  },
+  watch: true,
+  //devtool: true,
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '/dist/',
+            },
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: true,
+            }
+          },
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use:
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+        }
+      }
+    }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css',
+      })
+  ],
+};
